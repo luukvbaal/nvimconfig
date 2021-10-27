bdbf79d5081e2e0550bbc692d2cc854870ea7237
Luuk van Baal <luukvbaal@gmail.com>
2021-10-27 16:06:40 +0200

    nvim-cmp mappings, remove lua lsp workspace

--- init.lua
+++ init.lua
@@ -516,7 +516,6 @@ require("packer").startup({
 						Lua = {
 							runtime = { version = "LuaJIT", path = runtime_path },
 							diagnostics = { globals = { "vim" } },
-							workspace = { library = vim.api.nvim_get_runtime_file("", true) },
 						},
 					},
 				})
@@ -642,15 +641,31 @@ require("packer").startup({
 						end,
 					},
 					mapping = {
-						["<C-d>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-						["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-						["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-						["<C-y>"] = cmp.config.disable, -- If you want to remove the default `<C-y>` mapping, You can specify `cmp.config.disable` value.
-						["<C-e>"] = cmp.mapping({
-						  i = cmp.mapping.abort(),
-						  c = cmp.mapping.close(),
+						["<C-d>"] = cmp.mapping.scroll_docs(-4),
+						["<C-f>"] = cmp.mapping.scroll_docs(4),
+						["<C-e>"] = cmp.mapping.abort(),
+						["<CR>"] = cmp.mapping.confirm({
+							behavior = cmp.ConfirmBehavior.Replace,
+							select = true,
 						}),
-						["<CR>"] = cmp.mapping.confirm({ select = true }),
+						["<Tab>"] = function(fallback)
+							if cmp.visible() then
+								cmp.select_next_item()
+							elseif require("luasnip").expand_or_jumpable() then
+								vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
+							else
+								fallback()
+							end
+						end,
+						["<S-Tab>"] = function(fallback)
+							if cmp.visible() then
+								cmp.select_prev_item()
+							elseif require("luasnip").jumpable(-1) then
+								vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
+							else
+								fallback()
+							end
+						end,
 					}
 				})
 			cmp.setup.cmdline('/', {
