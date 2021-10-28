efbfe8635a4888557ba1a074df3703985731b71c
Luuk van Baal <luukvbaal@gmail.com>
2021-10-29 01:47:50 +0200

    rename prompt treesitter hightlight

--- init.lua
+++ init.lua
@@ -125,7 +125,7 @@ require("packer").startup({
 					auto_open = {
 						setup = "explorer",
 						tabpage = "explorer",
-						empty = true,
+						empty = false,
 						ft_ignore = { "gitcommit" }
 					},
 					auto_close = true,
@@ -487,7 +487,8 @@ require("packer").startup({
 				})
 			end,
 		})
-		use({ "folke/lua-dev.nvim" })
+		use({ "nvim-treesitter/playground", after = "nvim-treesitter" })
+		use("folke/lua-dev.nvim")
 		use({
 			"neovim/nvim-lspconfig",
 			after = "nvim-base16.lua",
@@ -921,64 +922,59 @@ function _G.vimgrepprompt()
 end
 
 function _G.rename()
-	local fn = vim.fn
-	local cword = vim.fn.expand("<cword>")
-	print(fn.synIDattr(fn.synIDtrans(fn.synID(fn.line("."), fn.col("."), 1)), "fg"))
-	local win = require('plenary.popup').create(cword, {
+	local rename = "textDocument/rename"
+	local currName = vim.fn.expand("<cword>")
+	local tshl = require("nvim-treesitter-playground.hl-info").get_treesitter_hl()
+	if tshl and #tshl > 0 then
+		local ind = tshl[#tshl]:match("^.*()%*%*.*%*%*")
+		tshl = tshl[#tshl]:sub(ind + 2, -3)
+	end
+
+	local win = require('plenary.popup').create(currName, {
 		title = "New Name",
 		style = "minimal",
 		borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
 		relative = "cursor",
 		borderhighlight = "FloatBorder",
 		titlehighlight = "TelescopePromptTitle",
-		highlight = "guifg="..fn.synIDattr(fn.synIDtrans(fn.synID(fn.line("."), fn.col("."), 1)), "fg"),
+		highlight = tshl,
 		focusable = true,
 		width = 25,
 		height = 1,
 		line = "cursor+2",
 		col = "cursor-1"
 	})
+
 	local map_opts = { noremap = true, silent = true }
-	vim.api.nvim_buf_set_keymap(0, 'i', '<Esc>', '<cmd>stopinsert | q!<CR>', map_opts)
-	vim.api.nvim_buf_set_keymap(0, 'n', '<Esc>', '<cmd>stopinsert | q!<CR>', map_opts)
-	vim.api.nvim_buf_set_keymap(0, 'i', '<CR>', "<cmd>stopinsert | lua _rename()<CR>", map_opts)
-	vim.api.nvim_buf_set_keymap(0, 'n', '<CR>', "<cmd>stopinsert | lua _rename()<CR>", map_opts)
+	vim.api.nvim_buf_set_keymap(0, "i", "<Esc>", "<cmd>stopinsert | q!<CR>", map_opts)
+	vim.api.nvim_buf_set_keymap(0, "n", "<Esc>", "<cmd>stopinsert | q!<CR>", map_opts)
+	vim.api.nvim_buf_set_keymap(0, "i", "<CR>", "<cmd>stopinsert | lua _rename('"..currName.."')<CR>", map_opts)
+	vim.api.nvim_buf_set_keymap(0, "n", "<CR>", "<cmd>stopinsert | lua _rename('"..currName.."')<CR>", map_opts)
 
 	local function handler(err, result, ctx, config)
-		local method
-		local isnew = not config or type(config) ~= "number"
-		if isnew then
-			method = ctx.method
-			result = result
-		else
-			method = result
-			result = ctx
-		end
 		if err then
-			vim.notify(("Error running lsp query '%s': %s"):format(method, err), vim.log.levels.ERROR)
+			vim.notify(("Error running lsp query '%s': %s"):format(rename, err), vim.log.levels.ERROR)
 		end
-		local new = ""
-		if result and result.changes then
+		local new
+		if result.changes then
 			local msg = ""
 			for f, c in pairs(result.changes) do
 				new = c[1].newText
 				msg = msg..("%d changes -> %s"):format(#c, f:gsub("file://",""):gsub(vim.fn.getcwd(),".")).."\n"
 				msg = msg:sub(1, #msg - 1)
-				vim.notify(msg, vim.log.levels.INFO, { title = ("Rename: %s -> %s"):format(cword, new) })
+				vim.notify(msg, vim.log.levels.INFO, { title = ("Rename: %s -> %s"):format(currName, new) })
 			end
 		end
-		vim.lsp.handlers[method](err, result, ctx, config)
+		vim.lsp.handlers[rename](err, result, ctx, config)
 	end
 
-	function _G._rename()
+	function _G._rename(curr)
 		local newName = vim.trim(vim.fn.getline('.'))
-		print(newName)
 		vim.api.nvim_win_close(win, true)
-		local currName = vim.fn.expand('<cword>')
-		if (newName and #newName > 0) and newName ~= currName then
+		if #newName > 0 and newName ~= curr then
 			local params = vim.lsp.util.make_position_params()
 			params.newName = newName
-			vim.lsp.buf_request(0, "textDocument/rename", params, handler)
+			vim.lsp.buf_request(0, rename, params, handler)
 		end
 	end
 end
