3e205a8efd701ab758e4ea742419e3200afe928e
Luuk van Baal <luukvbaal@gmail.com>
2022-06-01 00:51:31 +0200

    Update rename handler

--- init.lua
+++ init.lua
@@ -140,7 +140,7 @@ require("packer").startup({ function(use)
 				icon = "  ",
 			}
 			components.active[3][1] = {
-				provider = function() return vim.lsp.buf_get_clients()[1] and "  LSP" or "" end,
+				provider = function() return vim.lsp.get_active_clients() and "  LSP" or "" end,
 				enabled = function() return vim.api.nvim_win_get_width(0) > 70 end,
 				hl = { fg = colors.grey_fg, bg = colors.black2 },
 			}
@@ -361,19 +361,6 @@ require("packer").startup({ function(use)
 			vim.fn.sign_define("DiagnosticSignWarn", { text = "", texthl = "DiagnosticWarn" })
 			vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
 			vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
-			vim.lsp.handlers["textDocument/rename"] = function(err, result)
-				if err then vim.notify(("Error running lsp query 'rename': "..err), vim.log.levels.ERROR) end
-				if result and result.changes then
-					local msg = ""
-					for f, c in pairs(result.changes) do
-						local new = c[1].newText
-						msg = msg..("%d changes -> %s"):format(#c, f:gsub("file://",""):gsub(vim.fn.getcwd(),".")).."\n"
-						msg = msg:sub(1, #msg - 1)
-						vim.notify(msg, vim.log.levels.INFO, { title = ("Rename: %s -> %s"):format(vim.fn.expand("<cword>"), new) })
-					end
-				end
-				vim.lsp.util.apply_workspace_edit(result)
-			end
 			vim.lsp.buf.rename = {
 				float = function()
 					local currName = vim.fn.expand("<cword>")
@@ -405,16 +392,28 @@ require("packer").startup({ function(use)
 					local map_opts = { noremap = true, silent = true }
 					vim.api.nvim_buf_set_keymap(0, "i", "<Esc>", "<cmd>stopinsert | q!<CR>", map_opts)
 					vim.api.nvim_buf_set_keymap(0, "n", "<Esc>", "<cmd>stopinsert | q!<CR>", map_opts)
-					vim.api.nvim_buf_set_keymap(0, "i", "<CR>", "<cmd>stopinsert | lua vim.lsp.buf.rename.apply('"..currName..","..win.."')<CR>", map_opts)
-					vim.api.nvim_buf_set_keymap(0, "n", "<CR>", "<cmd>stopinsert | lua vim.lsp.buf.rename.apply('"..currName..","..win.."')<CR>", map_opts)
+					vim.api.nvim_buf_set_keymap(0, "i", "<CR>", "<cmd>stopinsert | lua vim.lsp.buf.rename.apply('"..currName.."',"..win..")<CR>", map_opts)
+					vim.api.nvim_buf_set_keymap(0, "n", "<CR>", "<cmd>stopinsert | lua vim.lsp.buf.rename.apply('"..currName.."',"..win..")<CR>", map_opts)
 				end,
 				apply = function(curr, win)
 					local newName = vim.trim(vim.fn.getline('.'))
-					vim.api.nvim_win_close(win, true)
+					vim.api.nvim_win_close(tonumber(win), true)
 					if #newName > 0 and newName ~= curr then
 						local params = vim.lsp.util.make_position_params()
 						params.newName = newName
-						vim.lsp.buf_request(0, "textDocument/rename", params)
+						vim.lsp.buf_request(0, "textDocument/rename", params, function(err, res, ctx)
+							if err then vim.notify(("Error running lsp query 'rename': "..err), vim.log.levels.ERROR) end
+							if res and res.changes then
+								local msg = ""
+								for f, c in pairs(res.changes) do
+									local new = c[1].newText
+									msg = msg..("%d changes -> %s"):format(#c, f:gsub("file://",""):gsub(vim.fn.getcwd(),".")).."\n"
+									msg = msg:sub(1, #msg - 1)
+									vim.notify(msg, vim.log.levels.INFO, { title = ("Rename: %s -> %s"):format(vim.fn.expand("<cword>"), new) })
+								end
+							end
+							vim.lsp.util.apply_workspace_edit(res, vim.lsp.get_client_by_id(ctx.client_id).offset_encoding)
+						end)
 					end
 				end
 			}
